2b0253e3b3fc1517e7e19a3a4ee3caf62bd6b065
DaveGutHib <120181687+DaveGutHib@users.noreply.github.com>
2023-02-02 19:33:25 +0000

    add damage scaling by combo count

--- damage.py
+++ damage.py
@@ -11,14 +11,33 @@
 x=0
 total = 0
 comboCount = 0
+damageScaling = 1.0
 
 
 while x != "q":
     x = input("Enter a number (press q to quit)")
     comboCount = comboCount + 1
+    if comboCount >= 10:
+        damageScaling = 0.1
+    elif comboCount == 9:
+        damageScaling = 0.2
+    elif comboCount == 8:
+        damageScaling = 0.3
+    elif comboCount == 7:
+        damageScaling = 0.4
+    elif comboCount == 6:
+        damageScaling = 0.5
+    elif comboCount == 5:
+        damageScaling = 0.6
+    elif comboCount == 4:
+        damageScaling = 0.7
+    elif comboCount == 3:
+        damageScaling = 0.8
+    else:
+        pass
     print("Combo Count", comboCount)
     print("X equals", x)
     print("Previous total", total)
-    total = total + int(x)
+    total = total + (damageScaling * int(x))
     print("New total", total)
 else: print("Thank you for playing. Final total is ", total)
