67ef2a68963e6bdbfac07c103553a2780ef6b309
DaveGutHib <120181687+DaveGutHib@users.noreply.github.com>
2023-02-02 14:58:38 +0000

    moved attack data from jsons to python files

--- run.py
+++ run.py
@@ -1,5 +1,10 @@
+from healthAmounts import healthAmount;
+
+
 userCharacter = ""; 
 # which character the user is playing as and wishing to check the damage for.
+opponentCharacter = "";
+# which character the user is doing damage to and calculating the remaining health for.
 opponentStartingHealth = 0;
 # global variable for the starting health of the opponent character. Will be a number between 850 and 1100.
 comboCount= 0;
@@ -7,11 +12,23 @@ comboCount= 0;
 healthCheck = 1;
 # at regular points the opponent health will be checked and then multiplied by this number.
 # at full health they will receive full damage (multiplied by 1), but this variable will decrease proportional to their health.
+settingUp = True;
+# condition intended to shut down the while loop
 stillCalculating = True;
 # condition intended to shut down the while loop
+implementedCharacters = {"Ryu", "Chun-Li", "Deejay"};
+# Unordered set containing characters currently with valid data for calculating
 
-while stillCalculating:
-        if userCharacter (input"Please enter your character name (Ryu, Deejay or Chun-Li")
+while settingUp:
+        if userCharacter (input("Please enter your character name (Ryu, Deejay or Chun-Li)")) in implementedCharacters:
+                #implment code to output list of character names
+
+                if opponentCharacter(input("Please enter your opponent\'s character")) in 
+
+                #implement code to output list of opponent names
+        else: 
+                print("Please enter a valid character name")
+
 
 
 # ryu = {
