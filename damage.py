80a1df105a7285712a45b680f81b289c5cbec189
DaveGutHib <120181687+DaveGutHib@users.noreply.github.com>
2023-02-02 19:17:17 +0000

    update basics of damage formula

--- damage.py
+++ damage.py
@@ -8,3 +8,17 @@
 #     print(i)
 
 # a.close()
+x=0
+total = 0
+comboCount = 0
+
+
+while x != "q":
+    x = input("Enter a number (press q to quit)")
+    comboCount = comboCount + 1
+    print("Combo Count", comboCount)
+    print("X equals", x)
+    print("Previous total", total)
+    total = total + int(x)
+    print("New total", total)
+else: print("Thank you for playing. Final total is ", total)
